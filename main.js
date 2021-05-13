51df0ac5f29d8f5a27fe2bbde56ad57878840dcf
shilichev <52910393+shilichev@users.noreply.github.com>
2021-05-13 12:07:58 +0300

    to fix server

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 let tableArray = [];
-
+const somefunc = async () => {
+  await fetch("http://localhost:5000/todos", {
+    method: "get",
+  }).then((response)=>console.log(response));
+};
+somefunc()
 const renderTable = () => {
   let table = document.getElementById("table");
   let trArray = "";
@@ -9,20 +14,35 @@ const renderTable = () => {
       let classTd = "";
       if (!td) {
         classTd = "nothing";
+        elem =
+          elem +
+          `<td width=${780 / tr.length + "px"} height=${
+            780 / tr.length + "px"
+          }  onclick="addValue()" class=${classTd} id=${
+            indexMain * 10 + index
+          }>${td}</td>`;
       } else if (td.length == 1) {
         classTd = "word";
+        elem =
+          elem +
+          `<td width=${780 / tr.length + "px"} height=${
+            780 / tr.length + "px"
+          }  onclick="addValue()" class=${classTd} id=${
+            indexMain * 10 + index
+          }>${td}</td>`;
       } else if (td == null) {
         return;
       } else {
         classTd = "quetion";
+
+        elem =
+          elem +
+          `<td width=${780 / tr.length + "px"} height=${
+            780 / tr.length + "px"
+          }  onclick="addValue()" class=${classTd} id=${
+            indexMain * 10 + index
+          }>${td}</td>`;
       }
-      elem =
-        elem +
-        `<td width=${780 / tableArray.length + "px"} height=${
-          780 / tableArray.length + "px"
-        }  onclick="addValue()" class=${classTd} id=${
-          indexMain * 10 + index
-        }>${td}</td>`;
     });
     trArray = trArray + `<tr>${elem}</tr>`;
   });
@@ -30,6 +50,7 @@ const renderTable = () => {
 };
 
 const createTable = (rowsCount, columnsCount) => {
+  tableArray = [];
   for (i = 0; i < rowsCount; i++) {
     tableArray.push([]);
   }
@@ -38,7 +59,7 @@ const createTable = (rowsCount, columnsCount) => {
   }
   renderTable();
 };
-createTable(10, 10);
+
 const getInfo = () => {
   let rows = Number(document.getElementById("row").value);
   let columns = Number(document.getElementById("column").value);
@@ -50,8 +71,9 @@ const getInfo = () => {
     createTable(rows, columns);
   }
 };
+
 const addValue = () => {
-  let value = prompt("Введите букву или вопрос");
+  document.getElementById(event.target.id).innerHTML = "<input size='5' />";
   console.log(event.target.id);
   tableArray.map((item, index) => {
     if (index === Math.floor(event.target.id / 10)) {
